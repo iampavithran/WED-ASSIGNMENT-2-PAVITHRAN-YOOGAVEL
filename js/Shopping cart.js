ed89cf3685b495ad82c57ab4239ac355d997ed54
iampavithran <122257436+iampavithran@users.noreply.github.com>
2023-03-21 14:36:31 +0800

    Add files via upload

---
+++ js/Shopping cart.js
@@ -0,0 +1,108 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'CHOCOLATE CHIP COOKIE',
+        image: '1.webp',
+        price: 15
+    },
+    {
+        id: 2,
+        name: 'CRINKLE COOKIE',
+        image: '2.webp',
+        price: 18
+    },
+    {
+        id: 3,
+        name: 'GINGER BREAD COOKIE',
+        image: '3.webp',
+        price: 25
+    },
+    {
+        id: 4,
+        name: 'OATMEAL RAISIN COOKIE',
+        image: '4.webp',
+        price: 20
+    },
+    {
+        id: 5,
+        name: 'PEANUT BUTTER COOKIE',
+        image: '5.webp',
+        price: 10
+    },
+    {
+        id: 6,
+        name: 'SNOWBALL COOKIE',
+        image: '6.webp',
+        price: 15
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        listCards[key] = products[key];
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    console.log(key, quantity);
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
